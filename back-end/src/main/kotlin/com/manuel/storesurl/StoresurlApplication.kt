d4758bb466dbcbb72960ddbf01df390a48b8644f
santiago gallego arias <mgallegoarias@gmail.com>
2020-11-30 13:38:31 -0500

    The back-en project, autogenerated with spring initializr and use the inmemory data base h2, Spring JPA for data acces, Spring Web, for easy test template mustache, the hateoas plug in for REST principles and Spring boot dev tools for local server

---
+++ back-end/src/main/kotlin/com/manuel/storesurl/StoresurlApplication.kt
@@ -0,0 +1,11 @@
+package com.manuel.storesurl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StoresurlApplication
+
+fun main(args: Array<String>) {
+	runApplication<StoresurlApplication>(*args)
+}
---
+++ back-end/src/test/kotlin/com/manuel/storesurl/StoresurlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.manuel.storesurl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StoresurlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
